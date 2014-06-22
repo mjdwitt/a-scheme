83dcd4f5447ec5ae9b36c9504cdd174e48e7a5be
Michael DeWitt <md@hackfoolery.com>
2014-06-21 18:55:58 -0700

    add some basic tests for parseScheme

---
+++ src/Scheme/Parser.hs
@@ -0,0 +1,78 @@
+module Scheme.Parser where
+
+import Data.Symbol
+import Text.Parsec
+import Text.Parsec.Expr
+import Text.Parsec.Language
+import Text.Parsec.String
+import Text.Parsec.Token
+
+
+
+-- We'll use Symbols for names in our language. Symbols aren't built in to 
+-- Haskell, but they can be easily added via the symbol package and created and 
+-- inspected using the @intern@ and @unintern@ functions in @Data.Symbol@.
+type Name = Symbol
+
+-- Our language will start as a basic lambda calculus plus integers.
+--
+--      Expr ::= int
+--             | var
+--             | (lambda (name) Expr)
+--             | (Expr Expr)
+--
+--  This BNF is isomorphic to the following Haskell algebraic datatype 
+--  definition:
+data Expr = Val Integer
+          | Var Name
+          | Lam Name Expr
+          | App Expr Expr
+  deriving (Show, Eq)
+
+-- (The last line in the definition of Expr there tells the compiler to 
+-- automatically derive an instance of the Show typeclass for our new Expr 
+-- datatype. This allows us to call the show function on any Expr--Haskell's 
+-- equivalent to many other language's toString.)
+
+
+
+-- Next, we'll define the rules for making identifiers in our language.
+lang = emptyDef{ identStart = letter
+               , identLetter = noneOf " \t\r\n\f\v()"
+               , reservedNames = ["lambda"]
+               , caseSensitive = True
+               }
+
+-- And then we'll use that definition to generate some tokenizers.
+TokenParser{ parens     = m_paren
+           , whiteSpace = m_space
+           , identifier = m_ident
+           , reserved   = m_reserved
+           , integer    = m_int
+           } = makeTokenParser lang
+
+-- makeTokenParser returns a larger record of type TokenParser. Here, we've 
+-- pattern-matched that return value against only the fields that we care about.  
+-- This gives us tokenizers for sexprs (m_parens), identifiers, (m_ident), 
+-- whitespace (m_space), and integers (m_int).
+
+
+
+-- Now to define the expression parser.
+scheme :: Parser Expr
+scheme = buildExpressionParser [] term <?> "expression"
+  where term =  do int <- m_int
+                   return $ Val int
+            <|> do var <- m_ident
+                   return . Var $ intern var
+            <|> m_paren ((do rator <- scheme
+                             rand <- m_space >> scheme
+                             return $ App rator rand)
+                     <|> (do m_reserved "lambda"
+                             ident <- m_paren m_ident
+                             let var = intern ident
+                             bod <- scheme
+                             return $ Lam var bod))
+
+parseScheme :: String -> Either ParseError Expr
+parseScheme = parse scheme "scheme"
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,5 +2,27 @@ module Main where
 
 import Test.Hspec
 
+import Data.Symbol
+import qualified Text.Parsec.Error
+
+import Scheme.Parser
+
+
+
+instance Eq Text.Parsec.Error.ParseError where
+  _ == _ = False
+
+
+
 main :: IO ()
-main = undefined
+main = hspec $ do
+
+  describe "parseScheme" $ do
+    it "parses integers" $ do
+      parseScheme "42" `shouldBe` Right (Val 42)
+    it "parses symbols" $ do
+      parseScheme "x" `shouldBe` Right (Var $ intern "x")
+    it "parses lambda sexprs" $ do
+      parseScheme "(lambda (x) x)" `shouldBe` Right (Lam (intern "x") (Var $ intern "x"))
+    it "parses application sexprs" $ do
+      parseScheme "(x x)" `shouldBe` Right (App (Var $ intern "x") (Var $ intern "x"))
