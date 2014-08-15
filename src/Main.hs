e7e77f9b67750f867899bdce0747cdfa534b8e4d
Michael DeWitt <md@hackfoolery.com>
2014-08-15 15:33:42 -0700

    wip; cleaning off rdio laptop

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,22 @@
 module Main where
 
+import Prelude hiding(print, putStrLn)
 import System.Environment(getArgs)
-import System.IO
+import System.IO.Streams
 
+import Repl
 import Scheme(eval)
+import Scheme.Types.Mocks(MockIO, print, putStrLn)
 
 
 
--- The driver function for the executable parser that simply runs the parser on 
--- the first command-line argument and prints the resulting error or Expr.
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [expr] -> printEval expr
-    []     -> repl
+    []     -> repl ',' printEval
     _      -> putStrLn "Invalid arguments"
 
-repl :: IO ()
-repl = do
-  putStr "> " >> hFlush stdout
-  input <- getLine
-  case input of
-    ',':command -> executeReplCommand command
-    expr -> printEval expr >> repl
-
-executeReplCommand :: String -> IO ()
-executeReplCommand "quit" = putStrLn "Bye bye."
-executeReplCommand "help" = do
-  putStrLn "Available commands:"
-  putStrLn ",quit: exit this repl session"
-  putStrLn ",help: print this help message"
-  repl
-executeReplCommand command = do
-  putStr "Invalid repl command "
-  print command
-  executeReplCommand "help"
-
-printEval :: String -> IO ()
+printEval :: MockIO io => String -> io ()
 printEval = either print print . eval
